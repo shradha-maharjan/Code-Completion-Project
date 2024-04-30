2315dee13539789ffa3097fd97bf340fdec950ba
Shradha Maharjan <shradhamaharjan@unomaha.edu>
2024-04-30 18:45:20 +0000

    Replace bart.py

--- CODE-SPT-Code/spt-code/sources/models/bart.py
+++ CODE-SPT-Code/spt-code/sources/models/bart.py
@@ -1,4 +1,3 @@
-
 import torch
 from torch.nn import CrossEntropyLoss, MSELoss
 import torch.nn.functional as f
@@ -16,9 +15,11 @@ from .utils import inputs_to_cuda
 
 logger = logging.getLogger(__name__)
 
+#BartForClassificationAndGeneration inherits from BartForConditionalGeneration, which is a BART (Bidirectional and Auto-Regressive Transformers) model specifically designed for text generation tasks.
 
 class BartForClassificationAndGeneration(BartForConditionalGeneration):
 
+# __init__ method, it sets up the classification head for the model. This includes creating an instance of BartClassificationHead, which will be used for text classification.
     def __init__(self, config: BartConfig, mode=None):
         super(BartForClassificationAndGeneration, self).__init__(config)
         self.mode = None
@@ -35,11 +36,14 @@ class BartForClassificationAndGeneration(BartForConditionalGeneration):
         self.model._init_weights(self.classification_head.dense)
         self.model._init_weights(self.classification_head.out_proj)
 
+#set_model_mode method is used to switch the mode of the BART model. Modes can be 'gen' (generation), 'cls' (classification), or 'search'.
     def set_model_mode(self, mode):
         assert mode in [enums.MODEL_MODE_GEN, enums.MODEL_MODE_CLS, enums.MODEL_MODE_SEARCH]
         self.mode = mode
         logging.info(f'BART mode switched to {mode}')
 
+#The forward method handles the forward pass of the model. Depending on the mode set (gen or others), it calls the appropriate forward method.
+#If the mode is set to 'gen', it calls the forward_gen method.
     def forward(
             self,
             input_ids=None,
@@ -64,7 +68,12 @@ class BartForClassificationAndGeneration(BartForConditionalGeneration):
         assert self.mode, 'It is required to specific a mode for BART before the model is passed through'
 
         if self.mode == enums.MODEL_MODE_GEN:
-
+            # print("Forwarding through mode 'gen'")
+            # print("Input IDs:", input_ids)
+            # print("Attention Mask:", attention_mask)
+            # print("Decoder Input IDs:", decoder_input_ids)
+            # print("Decoder Attention Mask:", decoder_attention_mask)
+            # print("Labels:", labels)
             return self.forward_gen(input_ids=input_ids,
                                     attention_mask=attention_mask,
                                     decoder_input_ids=decoder_input_ids,
@@ -84,6 +93,10 @@ class BartForClassificationAndGeneration(BartForConditionalGeneration):
         else:
             raise ValueError
 
+# This method performs the forward pass for text generation tasks.
+# It passes these inputs through the BART model and calculates the logits (scores) for each token in the vocabulary.
+# If labels are provided, it calculates the masked language model (MLM) loss based on the predicted logits and the actual labels.
+# It returns the outputs, including the loss, logits, and other relevant information.
     def forward_gen(
             self,
             input_ids=None,
@@ -103,6 +116,12 @@ class BartForClassificationAndGeneration(BartForConditionalGeneration):
             output_hidden_states=None,
             return_dict=None
     ):
+        # print("Forwarding through forward_gen")
+        # print("Input IDs:", input_ids)
+        # print("Attention Mask:", attention_mask)
+        # print("Decoder Input IDs:", decoder_input_ids)
+        # print("Decoder Attention Mask:", decoder_attention_mask)
+        # print("Labels:", labels)
         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
 
         if labels is not None:
@@ -128,17 +147,23 @@ class BartForClassificationAndGeneration(BartForConditionalGeneration):
             output_hidden_states=output_hidden_states,
             return_dict=return_dict,
         )
+        
         lm_logits = self.lm_head(outputs[0]) + self.final_logits_bias
+        # print("LM Logits Shape:", outputs[0].shape)
+        # print("Final Logits Bias Shape:", self.final_logits_bias.shape)
 
+#The masked language model loss is calculated using the CrossEntropyLoss between the predicted logits and the actual labels.
         masked_lm_loss = None
         if labels is not None:
             loss_fct = CrossEntropyLoss()
             masked_lm_loss = loss_fct(lm_logits.view(-1, self.config.vocab_size), labels.view(-1))
+            #print("Masked LM Loss:", masked_lm_loss.item())
 
         if not return_dict:
             output = (lm_logits,) + outputs[1:]
             return ((masked_lm_loss,) + output) if masked_lm_loss is not None else output
 
+#The function returns the masked language model loss and other relevant outputs.
         return Seq2SeqLMOutput(
             loss=masked_lm_loss,
             logits=lm_logits,
