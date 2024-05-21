27105495c2568336b61652a08a62d1da0bce1383
shradha.maharjan <shradha.maharjan@outlook.com>
2024-05-21 11:38:11 -0500

    Adding JDT files

--- CODE-SPT-Code/workspace-eclipse/sptcode_astparser/src/SPTCODEASTVisitor.java
+++ CODE-SPT-Code/workspace-eclipse/sptcode_astparser/src/SPTCODEASTVisitor.java
@@ -2,61 +2,96 @@ import org.eclipse.jdt.core.dom.*;
 
 public class SPTCODEASTVisitor extends ASTVisitor {
 
-    // Helper method to print messages
     private void print(String message) {
         System.out.println(message);
     }
 
-    @Override
     public boolean visit(MethodDeclaration node) {
         print("method_declaration");
-        return true; // Continue to visit children
+        return true; 
     }
 
-    @Override
     public boolean visit(IfStatement node) {
         print("if_statement__");
-        return true; // Continue to visit children
+        return true; 
     }
 
-    @Override
     public void endVisit(IfStatement node) {
         print("__if_statement");
     }
 
-    @Override
     public boolean visit(ReturnStatement node) {
         print("return_statement");
-        return true; // Continue to visit children
+        return true; 
     }
 
-    @Override
     public boolean visit(CastExpression node) {
         print("cast_expression");
-        return true; // Continue to visit children
+        return true; 
     }
 
-    @Override
     public boolean visit(ParenthesizedExpression node) {
         print("parenthesized_expression__");
-        return true; // Continue to visit children
+        return true; 
     }
 
-    @Override
     public void endVisit(ParenthesizedExpression node) {
         print("__parenthesized_expression");
     }
 
-    @Override
     public boolean visit(MethodInvocation node) {
         print("method_invocation");
-        return true; // Continue to visit children
+        return true; 
     }
 
-    @Override
     public boolean visit(InstanceofExpression node) {
         print("instanceof_expression");
-        return true; // Continue to visit children
+        return true; 
+    }
+
+    public boolean visit(VariableDeclarationStatement node) {
+        print("variable_declaration_statement");
+        return true; 
+    }
+
+    public boolean visit(ForStatement node) {
+        print("for_statement");
+        return true; 
+    }
+
+    public boolean visit(EnhancedForStatement node) {
+        print("enhanced_for_statement");
+        return true; 
+    }
+
+    public boolean visit(SwitchStatement node) {
+        print("switch_statement");
+        return true; 
+    }
+
+    public boolean visit(TryStatement node) {
+        print("try_statement");
+        return true; 
+    }
+
+    public boolean visit(CatchClause node) {
+        print("catch_clause");
+        return true; 
+    }
+    
+    public boolean visit(InfixExpression node) {
+        print("binary_expression");
+        return true; 
+    }
+    
+    public boolean visit(ConditionalExpression node) {
+        print("conditional_expression");
+        return true; 
+    }
+    
+    public boolean visit(DoStatement node) {
+        print("do_statement");
+        return true; 
     }
 
     public static void main(String[] args) {
