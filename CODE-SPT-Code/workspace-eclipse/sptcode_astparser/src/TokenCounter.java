2883e098e252570497f2f247e6c4e4ae378c6254
shradha.maharjan <shradha.maharjan@outlook.com>
2024-05-30 12:13:58 -0500

    Modified the code to add method calls count

--- CODE-SPT-Code/workspace-eclipse/sptcode_astparser/src/TokenCounter.java
+++ CODE-SPT-Code/workspace-eclipse/sptcode_astparser/src/TokenCounter.java
@@ -1,126 +1,115 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.io.FileWriter;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 import org.eclipse.jdt.core.JavaCore;
-import org.eclipse.jdt.core.compiler.IProblem;
 import org.eclipse.jdt.core.dom.*;
-
-//import MethodData;
-//import TokenCounterVisitorForMethods;
 
 public class TokenCounter {
 
-	private static String formatCode(String codeLine) {
-//	    System.out.println("Original: " + codeLine);
-		if (codeLine.startsWith("\"") && codeLine.endsWith("\"")) {
-			codeLine = codeLine.substring(1, codeLine.length() - 1);
-//	        System.out.println("Trimmed Quotes: " + codeLine);
-		}
-		codeLine = codeLine.replace("\\n", "\n").replace("\\\"", "\"");
-//	    System.out.println("Replaced Escapes: " + codeLine);
+    private static String formatCode(String codeLine) {
+        if (codeLine.startsWith("\"") && codeLine.endsWith("\"")) {
+            codeLine = codeLine.substring(1, codeLine.length() - 1);
+        }
+        codeLine = codeLine.replace("\\n", "\n").replace("\\\"", "\"");
+        return "public class DummyClass {\n" + codeLine + "\n}";
+    }
 
-		return "public class DummyClass {\n" + codeLine + "\n}";
-	}
+    public static void main(String[] args) {
+        String javaFileName = "input/methods.txt";
+        String outputFileName = "input/methods_outputs2.csv";
 
-	public static void main(String[] args) {
-		String javaFileName = "input/methods.txt";
-		String outputFileName = "input/methods_outputs2.csv"; // CSV output file path
+        Set<String> methodNames = new HashSet<>();
 
-		Set<String> methodNames = new HashSet<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(javaFileName));
+             FileWriter writer = new FileWriter(outputFileName)) {
 
-		try (BufferedReader reader = new BufferedReader(new FileReader(javaFileName));
-				FileWriter writer = new FileWriter(outputFileName)) {
+            writer.write("Index,Method Name,#Tokens,#Method Calls\n");
 
-			// Write the CSV header
-			writer.write("Index,Method Name,#Tokens\n");
+            String line;
+            int index = 1;
 
-			String line;
-			int index = 1;
+            while ((line = reader.readLine()) != null) {
+                String formattedCode = formatCode(line);
+                MethodData methodData = countTokens(formattedCode);
 
-			while ((line = reader.readLine()) != null) {
-				String formattedCode = formatCode(line);
-				MethodData methodData = countTokens(formattedCode);
+                String uniqueMethodName = methodData.methodName;
+                if (methodNames.contains(uniqueMethodName)) {
+                    uniqueMethodName += "_" + index; // Append index to make name unique
+                }
+                methodNames.add(uniqueMethodName);
 
-				String uniqueMethodName = methodData.methodName;
-				if (methodNames.contains(uniqueMethodName)) {
-					uniqueMethodName += "_" + index; // Append index to make name unique
-				}
-				methodNames.add(uniqueMethodName); // Add to set to track names
+                writer.write(index + "," + uniqueMethodName + "," + methodData.tokenCount + "," + methodData.methodCallCount + "\n");
+                index++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-				writer.write(index + "," + uniqueMethodName + "," + methodData.tokenCount + "\n");
-				index++;
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    private static MethodData countTokens(String source) {
+        ASTParser parser = ASTParser.newParser(AST.JLS14);
+        parser.setSource(source.toCharArray());
+        parser.setKind(ASTParser.K_COMPILATION_UNIT);
 
-	private static MethodData countTokens(String source) {
-		ASTParser parser = ASTParser.newParser(AST.JLS14);
-		parser.setSource(source.toCharArray());
-		parser.setKind(ASTParser.K_COMPILATION_UNIT);
+        Map<String, String> options = JavaCore.getOptions();
+        JavaCore.setComplianceOptions(JavaCore.VERSION_1_8, options);
+        parser.setCompilerOptions(options);
 
-		Map<String, String> options = JavaCore.getOptions();
-		JavaCore.setComplianceOptions(JavaCore.VERSION_1_8, options);
-		parser.setCompilerOptions(options);
+        CompilationUnit cu = (CompilationUnit) parser.createAST(null);
 
-		CompilationUnit cu = (CompilationUnit) parser.createAST(null);
+        TokenCounterVisitorForMethods visitor = new TokenCounterVisitorForMethods();
+        cu.accept(visitor);
 
-		TokenCounterVisitorForMethods visitor = new TokenCounterVisitorForMethods();
-		cu.accept(visitor);
-
-//        if (cu.getProblems().length > 0) {
-//            System.out.println("Compilation problems found:");
-//            for (IProblem problem : cu.getProblems()) {
-//                System.out.printf("Error at line %d, column %d: %s\n", 
-//                    problem.getSourceLineNumber(), 
-//                    problem.getSourceStart(), 
-//                    problem.getMessage());
-//            }
-//        }
-
-		return new MethodData(visitor.getMethodName(), visitor.getTokenCount());
-	}
+        return new MethodData(visitor.getMethodName(), visitor.getTokenCount(), visitor.getMethodCallCount());
+    }
 }
 
 class TokenCounterVisitorForMethods extends ASTVisitor {
-	private int tokenCount = 0;
-	private String methodName = null;
+    private int tokenCount = 0;
+    private int methodCallCount = 0;
+    private String methodName = null;
 
-	public boolean visit(MethodDeclaration node) {
-		this.methodName = node.getName().getIdentifier(); // Capture the method name when visiting
-//      System.out.println("Visiting MethodDeclaration: " + node.getName());
-		return true;
-	}
+    public boolean visit(MethodDeclaration node) {
+        this.methodName = node.getName().getIdentifier();
+        return true;
+    }
 
-	public boolean visit(SimpleName node) {
-		tokenCount++;
-		return true;
-	}
+    public boolean visit(MethodInvocation node) {
+        methodCallCount++;
+        return true;
+    }
 
-	public int getTokenCount() {
-		return tokenCount;
-	}
+    public boolean visit(SimpleName node) {
+        tokenCount++;
+        return true;
+    }
 
-	public String getMethodName() {
-		return methodName;
-	}
+    public int getTokenCount() {
+        return tokenCount;
+    }
+
+    public int getMethodCallCount() {
+        return methodCallCount;
+    }
+
+    public String getMethodName() {
+        return methodName;
+    }
 }
 
 class MethodData {
-	String methodName;
-	int tokenCount;
+    String methodName;
+    int tokenCount;
+    int methodCallCount;
 
-	MethodData(String methodName, int tokenCount) {
-		this.methodName = methodName;
-		this.tokenCount = tokenCount;
-	}
+    MethodData(String methodName, int tokenCount, int methodCallCount) {
+        this.methodName = methodName;
+        this.tokenCount = tokenCount;
+        this.methodCallCount = methodCallCount;
+    }
 }
