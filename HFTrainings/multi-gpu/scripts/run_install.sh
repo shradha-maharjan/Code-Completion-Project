602680a427b1f16baf14b938d6aade0414c481b9
Myoungkyu Song <myoungkyu@unomaha.edu>
2024-05-29 23:11:54 -0500

    Updated at Wed May 29 23:11:54 CDT 2024

---
+++ HFTrainings/multi-gpu/scripts/run_install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+pip3 install tqdm
+conda install -y -c conda-forge git-lfs
+git config --global user.email 'rshakywar@unomaha.edu'
+git config --global user.name 'Rajul Shakywar'
+python -m pip install scikit-learn transformers datasets sentencepiece sacremoses accelerate
+pip3 install --upgrade huggingface_hub
+pip3 install 'huggingface_hub[cli,torch]'
+pip3 install ipywidgets
+pip3 install numpy pandas matplotlib
+pip3 install ipykernel
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+pip3 install prettytable
+echo 'Installation completed'
