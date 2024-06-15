5f7d2ebc610b6cd41cc97f9b312451fd2c2674ff
Your Name <you@example.com>
2024-06-14 23:56:02 -0500

    Updated at Fri Jun 14 11:56:02 PM CDT 2024

--- HFTrainings/multi-gpu/scripts/run_install.sh
+++ HFTrainings/multi-gpu/scripts/run_install.sh
@@ -2,8 +2,8 @@
 
 pip3 install tqdm
 conda install -y -c conda-forge git-lfs
-git config --global user.email 'rshakywar@unomaha.edu'
-git config --global user.name 'Rajul Shakywar'
+git config --global user.email 'myoungkyu@unomaha.edu'
+git config --global user.name 'Myoungkyu Song'
 python -m pip install scikit-learn transformers datasets sentencepiece sacremoses accelerate
 pip3 install --upgrade huggingface_hub
 pip3 install 'huggingface_hub[cli,torch]'
@@ -12,6 +12,12 @@ pip3 install numpy pandas matplotlib
 pip3 install ipykernel
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 pip3 install prettytable
+echo '--------------------------------------------------------'
 echo 'Installation completed'
+echo '--------------------------------------------------------'
 
-# This script should be updated to use a ``conda virtual environment``.
+# conda env create -f env_mlm.yml
+# echo $SHELL
+# conda init bash
+# # source .bashrc
+# conda activate env_mlm
