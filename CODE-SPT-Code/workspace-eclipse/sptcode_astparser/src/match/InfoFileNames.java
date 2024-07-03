41f2eac35dc6a7cae63be13c736153b666bb9724
shradha.maharjan <shradha.maharjan@outlook.com>
2024-07-03 09:21:24 -0500

    changes

--- CODE-SPT-Code/workspace-eclipse/sptcode_astparser/src/match/InfoFileNames.java
+++ CODE-SPT-Code/workspace-eclipse/sptcode_astparser/src/match/InfoFileNames.java
@@ -13,22 +13,22 @@ public interface InfoFileNames {
    // static String FILE_RAW_METHODS = "";
    // Output files
 
-   static String FILE_PRE_METHODS = "input/inputdebug-pre.txt";// "input/step0-valid.source.txt";//
-   static String FILE_RAW_METHODS = "input/inputdebug-raw.txt";// "input/step0-raw-methods.txt";
-//    static String FILE_PRE_METHODS = "input/step0-valid.source.txt";//
-//    static String FILE_RAW_METHODS = "input/step0-raw-methods.txt";
+//   static String FILE_PRE_METHODS = "input/inputdebug-pre.txt";// "input/step0-valid.source.txt";//
+//   static String FILE_RAW_METHODS = "input/inputdebug-raw.txt";// "input/step0-raw-methods.txt";
+    static String FILE_PRE_METHODS = "input/step0-valid.source.txt";//
+    static String FILE_RAW_METHODS = "input/step0-raw-methods.txt";
 
    // Testing
    // Include 10 methods in FILE_RAW_METHODS
    // Include 2 methods in FILE_PRE_METHODS
 
-//    static String FILE_MATCHED_METHODS = "output/step1-matched.txt";
-//    static String FILE_UNMATCHED_METHODS_PRE = "output/step1-unmatched-pre.txt";
-//    static String FILE_UNMATCHED_METHODS_RAW =  "output/step1-unmatched-raw.txt";
-
-   static String FILE_MATCHED_METHODS = "output/outputdebug-matched.txt";// "output/step1-raw-pre.txt";
-   static String FILE_UNMATCHED_METHODS_PRE = "output/outputdebug-unmatched_pre.txt";// "output/step1-raw-pre.txt";
-   static String FILE_UNMATCHED_METHODS_RAW = "output/outputdebug-unmatched_raw.txt";// "output/step1-raw-pre.txt";
+    static String FILE_MATCHED_METHODS = "output/step1-matched.txt";
+    static String FILE_UNMATCHED_METHODS_PRE = "output/step1-unmatched-pre.txt";
+    static String FILE_UNMATCHED_METHODS_RAW =  "output/step1-unmatched-raw.txt";
+//
+//   static String FILE_MATCHED_METHODS = "output/output1-matched.txt";// "output/step1-raw-pre.txt";
+//   static String FILE_UNMATCHED_METHODS_PRE = "output/output1-unmatched_pre.txt";// "output/step1-raw-pre.txt";
+//   static String FILE_UNMATCHED_METHODS_RAW = "output/output1-unmatched_raw.txt";// "output/step1-raw-pre.txt";
   // static String FILE_MATCHED_METHODS = "output/step1-raw-pre.txt";
 
 }
