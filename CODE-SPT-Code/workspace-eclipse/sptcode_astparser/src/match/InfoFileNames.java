9a72cb5a3b1cd00412845c5a63890d14133af573
shradhamaharjan <shradhamaharjan@unomaha.edu>
2024-07-05 18:33:05 +0100

    changes

--- CODE-SPT-Code/workspace-eclipse/sptcode_astparser/src/match/InfoFileNames.java
+++ CODE-SPT-Code/workspace-eclipse/sptcode_astparser/src/match/InfoFileNames.java
@@ -6,29 +6,29 @@ public interface InfoFileNames {
 	// Assume that the files below were sorted.
 	// Small sample datasets
 	// After removing the first matching.
-	// static String FILE_PRE_METHODS = "input/step1-valid.source.txt";
-	// static String FILE_RAW_METHODS = "input/step1-raw-methods.txt";
+	 static String FILE_PRE_METHODS = "input/step1-test.source.txt";
+	 static String FILE_RAW_METHODS = "input/step1-test.raw.methods.txt";
 	// Large datasets
 	// static String FILE_PRE_METHODS = "";
 	// static String FILE_RAW_METHODS = "";
 	// Output files
 
-	static String FILE_PRE_METHODS = "input/inputdebug-pre.txt";// "input/step0-valid.source.txt";//
-	static String FILE_RAW_METHODS = "input/inputdebug-raw.txt";// "input/step0-raw-methods.txt";
-//    static String FILE_PRE_METHODS = "input/step0-valid.source.txt";//
-//    static String FILE_RAW_METHODS = "input/step0-raw-methods.txt";
+//	static String FILE_PRE_METHODS = "input/inputdebug-pre.txt";// "input/step0-valid.source.txt";//
+//	static String FILE_RAW_METHODS = "input/inputdebug-raw.txt";// "input/step0-raw-methods.txt";
+//    static String FILE_PRE_METHODS = "input/step0-test.source.txt";//
+//    static String FILE_RAW_METHODS = "input/step0-test.raw.methods.txt";
 
-	// Testing
-	// Include 10 methods in FILE_RAW_METHODS
-	// Include 2 methods in FILE_PRE_METHODS
 
-//    static String FILE_MATCHED_METHODS = "output/step1-matched.txt";
-//    static String FILE_UNMATCHED_METHODS_PRE = "output/step1-unmatched-pre.txt";
-//    static String FILE_UNMATCHED_METHODS_RAW =  "output/step1-unmatched-raw.txt";
+	 static String FILE_MATCHED_METHODS = "output/step2-test-matched.txt";
+	 static String FILE_UNMATCHED_METHODS_PRE = "output/step2-test-unmatched-pre.txt";
+	 static String FILE_UNMATCHED_METHODS_RAW =  "output/step2-test-unmatched-raw.txt";
+//    static String FILE_MATCHED_METHODS = "output/step1-test-matched.txt";
+//    static String FILE_UNMATCHED_METHODS_PRE = "output/step1-test-unmatched-pre.txt";
+//    static String FILE_UNMATCHED_METHODS_RAW =  "output/step1-test-unmatched-raw.txt";
 //
-	static String FILE_MATCHED_METHODS = "output/output1-matched.txt";// "output/step1-raw-pre.txt";
-	static String FILE_UNMATCHED_METHODS_PRE = "output/output1-unmatched_pre.txt";// "output/step1-raw-pre.txt";
-	static String FILE_UNMATCHED_METHODS_RAW = "output/output1-unmatched_raw.txt";// "output/step1-raw-pre.txt";
+//	static String FILE_MATCHED_METHODS = "output/output1-matched.txt";// "output/step1-raw-pre.txt";
+//	static String FILE_UNMATCHED_METHODS_PRE = "output/output1-unmatched_pre.txt";// "output/step1-raw-pre.txt";
+//	static String FILE_UNMATCHED_METHODS_RAW = "output/output1-unmatched_raw.txt";// "output/step1-raw-pre.txt";
 	// static String FILE_MATCHED_METHODS = "output/step1-raw-pre.txt";
 
 }
