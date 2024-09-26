4851ba0d8daa92fd39fd296e8824e932e701ec40
Shradha <shradhamaharjan@unomaha.edu>
2024-09-26 12:47:37 -0500

    added README

---
+++ backup_script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+tar -cvf CODE-SPT-Code.tar CODE-SPT-Code && mv CODE-SPT-Code.tar /media/user1-selab3/data03/data_shradha/backup-2024-0926-1200pm/
+tar -cvf CODE-SPT-Code-TreeSitter.tar CODE-SPT-Code-TreeSitter && mv CODE-SPT-Code-TreeSitter.tar /media/user1-selab3/data03/data_shradha/backup-2024-0926-1200pm/
+tar -cvf data_shradha.tar data_shradha && mv data_shradha.tar /media/user1-selab3/data03/data_shradha/backup-2024-0926-1200pm/
+tar -cvf deploy-spt-code.tar deploy-spt-code && mv deploy-spt-code.tar /media/user1-selab3/data03/data_shradha/backup-2024-0926-1200pm/
+tar -cvf kube.tar kube && mv kube.tar /media/user1-selab3/data03/data_shradha/backup-2024-0926-1200pm/
+
